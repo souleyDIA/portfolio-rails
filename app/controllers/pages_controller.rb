01c69e8a03b7ec162ca0789ca8c40a6445012c2c
codecademy <ccuser@codecademy.com>
2022-11-22 09:42:13 +0000

    Added about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
   def welcome
   end
-  
+
   def page
     @projects = Project.all
   end
+
+  def about
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 get "/" => "pages#welcome", as: :welcome
 get "pages" => "pages#page", as: :page
+get "about" => "pages#about", as: :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
